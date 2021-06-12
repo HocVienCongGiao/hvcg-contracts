e768d2069f362cd31c3175046ec13e881a0c52ce
Peter Bean <binh@sunrise.vn>
2021-06-13 00:21:01 +0700

    WIP, ok?

---
+++ scripts/gen-api.sh
@@ -0,0 +1,1 @@
+java -jar ./bin/openapi-generator-cli.jar generate -i ./contracts/example/openapi/entity/v1.yaml -g rust-server -o rust_api_server-custom_config --package-name=hvcg_example_openapi_entity
